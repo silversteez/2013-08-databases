c44914fa4d0db7f849be1cf30aec919a31f286b3
Hack Reactor Students <github@hackreactor.com>
2013-08-29 19:52:22 -0700

    SQL file working

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -4,6 +4,11 @@ USE chat;
 
 CREATE TABLE messages (
  /* Describe your table here.*/
+ id integer NOT NULL AUTO_INCREMENT,
+ username varchar(15),
+ text varchar(140),
+ roomname varchar(15),
+ PRIMARY KEY (id)
 );
 
 /* You can also create more tables, if you need them... */
